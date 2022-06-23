da6785a987ec02888bf69e9078fe4efa423afef0
HaNeul-Kim <hskimsky@gmail.com>
2022-06-23 23:12:31 +0900

    build script 추가

---
+++ build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+if [ $# -lt 1 ] ; then
+  echo "Usage: $0 </absolute/path/to/hbase/home>"
+fi
+
+HBASE_HOME=$1
+mkdir build
+cd build
+cmake -D DOWNLOAD_DEPENDENCIES=ON -D HBASE_HOME=${HBASE_HOME} ..
